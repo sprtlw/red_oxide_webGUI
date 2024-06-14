d54b44c686a1f529475dc8ff7e95d44f9e06946f
Sam <sprtlw@pm.me>
2024-06-13 21:15:42 -0400

    multiple urls

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import subprocess
 import uuid
 import secrets
 
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, request, session, jsonify
 from flask_socketio import SocketIO, join_room, leave_room
 from decouple import config
 
@@ -12,9 +12,57 @@ app.config['SECRET_KEY'] = secrets.token_hex(
 
 socketio = SocketIO(app)
 
+url_list = []
+
+
+@app.route('/add_url', methods=['POST'])
+def add_url():
+    # Get JSON data
+    data = request.get_json()
+    new_url = data.get('url')
+    if new_url:
+        url_list.append(new_url)
+    return jsonify(url_list=url_list)
+
+
+@app.route('/remove_url', methods=['POST'])
+def remove_url():
+    # Get JSON data
+    data = request.get_json()
+    url_to_remove = data.get('url')
+    if url_to_remove in url_list:
+        url_list.remove(url_to_remove)
+    return jsonify(url_list=url_list)
+
+
+def process_urls_and_run_commands(api_key, input_dir, transcode_dir, torrent_dir, spectrogram_dir, selected_formats, session_id):
+    # Loop through URL list and create command
+    for url in url_list:
+        command = [
+            "red_oxide",
+            "transcode",
+            "--api-key", api_key,
+            "--content-directory", input_dir,
+            "--transcode-directory", transcode_dir,
+            "--torrent-directory", torrent_dir,
+            "--spectrogram-directory", spectrogram_dir,
+            "--skip-spectrogram",
+        ]
+        for file_format in selected_formats:
+            command.extend(["-f", file_format])
+        command.extend(["-m", "-a", "-d", url])
+        print(command)
+        # Start background task to run command
+        socketio.start_background_task(
+            target=run_command, command=command, session_id=session_id)
+
+    # Clear URL list after processing
+    url_list.clear()
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    # Generate a unique session ID for each request
     if request.method == 'POST':
         session_id = str(uuid.uuid4())
         session['id'] = session_id
@@ -25,64 +73,58 @@ def index():
         transcode_dir = "Z:/data/torrents/music/tmp/transcodes"
         torrent_dir = "Z:/data/torrents/music/watch"
         spectrogram_dir = "Z:/data/torrents/music/tmp/spectrograms"
-        permalink = request.form['permalink']
 
-        for audio_format in selected_formats:
-            command = [
-                "red_oxide",
-                "transcode",
-                "--api-key", api_key,
-                "--content-directory", input_dir,
-                "--transcode-directory", transcode_dir,
-                "--torrent-directory", torrent_dir,
-                "--spectrogram-directory", spectrogram_dir,
-                "--skip-spectrogram",
-                "-f", audio_format,
-                "-m",
-                "-a",
-                # "-d",
-                permalink
-            ]
-            socketio.start_background_task(
-                target=run_command, command=command, session_id=session_id)
+        # Process the submitted URLs and run commands for transcoding
+        process_urls_and_run_commands(
+            api_key, input_dir, transcode_dir, torrent_dir, spectrogram_dir, selected_formats, session_id)
 
         return render_template('index.html', session_id=session_id)
     else:
+        # Generate a new session ID for GET requests
         session_id = str(uuid.uuid4())
         session['id'] = session_id
         return render_template('index.html', session_id=session_id)
 
 
 def run_command(command, session_id):
+    # Run subprocess command
     process = subprocess.Popen(command, stdout=subprocess.PIPE,
                                stderr=subprocess.PIPE, text=True, encoding='utf-8', errors='replace')
-    for line in iter(process.stdout.readline, ''):
+
+    # Read and emit command output to session
+    for line in process.stdout:
+        print(line)
         socketio.emit('command_output', {'data': line}, to=session_id)
-    for line in iter(process.stderr.readline, ''):
+
+    for line in process.stderr:
+        print(line)
         socketio.emit('command_output', {'data': line}, to=session_id)
-    process.stdout.close()
-    process.stderr.close()
+
     process.wait()
 
 
 @socketio.on('connect')
 def handle_connect():
+    # Handle client connection
     print('Client connected')
 
 
 @socketio.on('disconnect')
 def handle_disconnect():
+    # Handle client disconnection
     print('Client disconnected')
 
 
 @socketio.on('join')
 def on_join(data):
+    # Join a room
     session_id = data['session_id']
     join_room(session_id)
 
 
 @socketio.on('leave')
 def on_leave(data):
+    # Leave a room
     session_id = data['session_id']
     leave_room(session_id)
 
--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
+/*$(document).ready(function () {
   function handleMp3Change() {
     if ($('#mp3320').is(':checked') || $('#mp3-v0').is(':checked')) {
       $('#flac').prop('checked', false)
@@ -23,21 +23,81 @@ $(document).ready(function () {
   handleMp3Change()
   handleFlacChange()
 })
+*/
 
 $(document).ready(function () {
+  // Initialize socket connection
   var socket = io.connect('http://' + document.domain + ':' + location.port)
   var sessionId = $('#session-id').val()
 
+  // Emit join event when socket connects
   socket.on('connect', function () {
     socket.emit('join', { session_id: sessionId })
   })
 
+  // Display command output received from server
   socket.on('command_output', function (msg) {
     $('#command-output').append('<p>' + msg.data + '</p>')
   })
 
+  // Submit form to clear output and emit leave event
   $('#transcode-form').submit(function () {
     $('#command-output').empty() // Clear the output container
     socket.emit('leave', { session_id: sessionId })
+  })
+})
+
+$(document).ready(function () {
+  // Function to add a new URL
+  function addUrl() {
+    const newUrl = $('#new-url').val()
+    $.ajax({
+      type: 'POST',
+      url: '/add_url',
+      data: JSON.stringify({ url: newUrl }),
+      contentType: 'application/json',
+      success: function (response) {
+        $('#url-list').html('')
+        response.url_list.forEach(function (url) {
+          $('#url-list').append(
+            '<p class="url-item" data-url="' + url + '">' + url + '</p>'
+          )
+        })
+        $('#new-url').val('')
+      },
+    })
+  }
+
+  // Click event for adding URL
+  $('#add-url-button').click(function () {
+    addUrl()
+  })
+
+  // Keypress event for adding URL on Enter key press
+  $('#new-url').keypress(function (e) {
+    if (e.which == 13) {
+      // Enter key pressed
+      addUrl()
+      return false // Prevent form submission
+    }
+  })
+
+  // Click event for removing URL
+  $(document).on('click', '.url-item', function () {
+    const urlToRemove = $(this).data('url')
+    $.ajax({
+      type: 'POST',
+      url: '/remove_url',
+      data: JSON.stringify({ url: urlToRemove }),
+      contentType: 'application/json',
+      success: function (response) {
+        $('#url-list').html('')
+        response.url_list.forEach(function (url) {
+          $('#url-list').append(
+            '<p class="url-item" data-url="' + url + '">' + url + '</p>'
+          )
+        })
+      },
+    })
   })
 })
